fc75eed9868933b168c476920fe84c001471f411
Phelix <dev.phelixochieng@gmail.com>
2025-06-12 14:37:19 +0300

    feat: add nuxtjs

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+const host = process.env.TAURI_DEV_HOST;
+
+export default defineNuxtConfig({
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  ssr: false,
+  devServer: { host: host || "localhost", port: 4000 },
+  vite: {
+    clearScreen: true,
+    envPrefix: ["VITE_", "TAURI_"],
+    server: {
+      strictPort: true,
+      hmr: host
+        ? {
+            protocol: "ws",
+            host,
+            port: 1421,
+          }
+        : undefined,
+      watch: {
+        ignored: ["**/src-tauri/**"],
+      },
+    },
+  },
+});
